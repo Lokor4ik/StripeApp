9b36eca44f3eeaf11136061a7d30225c03f81f32
Lokor4ik <lokorinfantem@gmail.com>
2020-12-28 16:46:58 +0200

    added Stripe wrapper

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,6 +1,9 @@
 import Head from 'next/head';
 import PropsTypes from 'prop-types';
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
+const stripePromise = loadStripe(process.env.PUBLISHABLE_KEY);
 export default function MainLayout({ children, title }) {
   return (
     <div>
@@ -9,7 +12,7 @@ export default function MainLayout({ children, title }) {
       </Head>
 
       <main>
-        {children}
+        <Elements stripe={stripePromise}>{children}</Elements>
       </main>
     </div>
   );
