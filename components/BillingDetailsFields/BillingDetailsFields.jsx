765f73169032bc5a6465cb9f059325eaab7f5c35
Lokor4ik <lokorinfantem@gmail.com>
2020-12-28 16:47:38 +0200

    improved BilingDetails

--- components/BillingDetailsFields/BillingDetailsFields.jsx
+++ components/BillingDetailsFields/BillingDetailsFields.jsx
@@ -1,4 +1,4 @@
-import FormField from "./FormField";
+import FormField from "components/common/FormField/FormField";
 
 export default function BillingDetailsFields() {
   return (
@@ -8,14 +8,12 @@ export default function BillingDetailsFields() {
         label="Name"
         type="text"
         placeholder="Jane Doe"
-        required
       />
       <FormField
         name="email"
         label="Email"
         type="email"
         placeholder="jane.doe@example.com"
-        required
       />
       <FormField
         name="address"
@@ -29,21 +27,18 @@ export default function BillingDetailsFields() {
         label="City"
         type="text"
         placeholder="San Francisco"
-        required
       />
       <FormField
         name="state"
         label="State"
         type="text"
         placeholder="California"
-        required
       />
       <FormField
         name="zip"
         label="ZIP"
         type="text"
         placeholder="94103"
-        required
       />
     </>
   );
